d4c1ecef097c4c0ca0ea3474aa17f5b95dcfd408
isamposebe <76258371+isamposebe@users.noreply.github.com>
2024-05-27 16:01:08 +0300

    Исправил заголовок чтобы был читабелен

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Moex Data Loader</title>
 </head>
 <body>
-    <h1>Загрузить данные о сделках на валютном рынке с сайта https://www.moex.com/ru/orders?historicaldata (Валютный рынок (csv.zip, 3,3 Мб))</h1>
+    <h1>Валютный рынок (csv.zip, 3,3 Мб)</h1>
     <form action="load_data.php" method="post">
         <button type="submit">Загрузить данные</button>
     </form>
