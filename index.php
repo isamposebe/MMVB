bebee7ede1d35917dae4f477a214e6c338fb9080
isamposebe <76258371+isamposebe@users.noreply.github.com>
2024-05-27 17:39:43 +0300

    Переписал запрос

--- index.php
+++ index.php
@@ -21,16 +21,17 @@
     <h1>Валютный рынок (csv.zip, 3,3 Мб)</h1>
 
     <?php
+    // Подключаемся к базе данных SQLite
     try {
-        // Подключаемся к базе данных SQLite
         $db = new SQLite3('moex_data.db');
-        // Выполняем запрос для получения всех данных из таблицы Trades
-        $results = $db->query('SELECT * FROM Trades');
-        if (!$results) {
-            die("Error executing query: " . $db->lastErrorMsg());
-        }
     } catch (Exception $e) {
         die("Error connecting to database: " . $e->getMessage());
+    }
+
+    // Выполняем запрос для получения всех данных из таблицы Trades
+    $results = $db->query('SELECT * FROM Trades');
+    if (!$results) {
+        die("Error executing query: " . $db->lastErrorMsg());
     }
     ?>
 
