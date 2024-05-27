51cc81ea16fe6f3cce574e8bf5e0931836d173c6
isamposebe <76258371+isamposebe@users.noreply.github.com>
2024-05-27 15:06:14 +0300

    Добавик кнопку

--- index.php
+++ index.php
@@ -5,5 +5,8 @@
 </head>
 <body>
     <h1>Загрузить данные о сделках на валютном рынке с сайта https://www.moex.com/ru/orders?historicaldata (Валютный рынок (csv.zip, 3,3 Мб))</h1>
+    <form action="load_data.php" method="post">
+        <button type="submit">Загрузить данные</button>
+    </form>
 </body>
 </html>
