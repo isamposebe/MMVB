8bae51885fffe1ea68f2b11a982ada3ae03c444b
isamposebe <76258371+isamposebe@users.noreply.github.com>
2024-05-27 15:21:25 +0300

    Функция для скачивания файла по URL

---
+++ load_data.php
@@ -0,0 +1,21 @@
+<?php
+    // Функция для скачивания файла по URL
+    function downloadFile($url, $path) {
+        $newfname = $path;
+        $file = fopen($url, 'rb');
+        if ($file) {
+            $newf = fopen($newfname, 'wb');
+            if ($newf) {
+                while (!feof($file)) {
+                    fwrite($newf, fread($file, 1024 * 8), 1024 * 8);
+                }
+            }
+        }
+        if ($file) {
+            fclose($file);
+        }
+        if ($newf) {
+            fclose($newf);
+        }
+    }
+?>
