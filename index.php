4f8c820f6e1dffae93268969d6b9071e0b6578c7
isamposebe <76258371+isamposebe@users.noreply.github.com>
2024-05-27 14:52:42 +0300

    Добавлен шаблон
    Написано что должно быть сделано

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Moex Data Loader</title>
+</head>
+<body>
+    <h1>Загрузить данные о сделках на валютном рынке с сайта https://www.moex.com/ru/orders?historicaldata (Валютный рынок (csv.zip, 3,3 Мб))</h1>
+</body>
+</html>
